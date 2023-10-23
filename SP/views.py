24395cb65da4bf3386b87af88d3f1c404d18c722
XEGENDvy <vishalsyadav4@gmail.com>
2023-10-23 10:30:57 +0530

    product funtion added

--- SP/views.py
+++ SP/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+def products(request):
+    return render(request,'products.html')
