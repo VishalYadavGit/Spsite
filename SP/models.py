9655a148ec88578ec35ab7dc254e5e1282ea66fa
XEGENDvy <vishalsyadav4@gmail.com>
2023-10-25 11:44:25 +0530

    updated models with machine name in __str__

--- SP/models.py
+++ SP/models.py
@@ -5,6 +5,8 @@ from django.db import models
 class Machine_category(models.Model):
     Category_name=models.CharField(max_length=100)
     Category_img=models.ImageField(default=None)
+    def __str__(self):
+        return self.Category_name
 
 class Machine(models.Model):
     category=models.ForeignKey(Machine_category,on_delete=models.CASCADE,related_name="machines")
